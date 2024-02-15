ccc6fc49391f734691f3b88e28fcd63b870892e7
Imran Chowdhury <144194950+imranncc@users.noreply.github.com>
2024-02-15 14:31:09 +0530

    Add files via upload

---
+++ src/TwoSum.java
@@ -0,0 +1,29 @@
+/* Assignment 5 (100 marks in total; 5% of the final score of this course)
+ *
+ * Question 2 (30 marks)
+ *
+ * Write a subroutine, user input an 1D array of integers and an integer target,
+ * return indices of the two numbers such that they add up to target.
+ *
+ * INPUT: user input array and target
+ * OUTPUT: the indices of the two numbers such that they add up to target.
+ *
+ * The array and target are parameters to the subroutine.
+ * User can get hint to input array and target number, respectively.
+ *
+ * Keep the input format in the example and make your code user-friendly.
+ *
+ * Example:
+ * INPUT: [2,7,11,15], 9
+ * OUTPUT: [0,1]
+ *
+ */
+
+public class Q2_TwoSum {
+    /* place your subroutine code here */
+
+    public static void main(String[] args) {
+        /* place your code to run your subroutine here */
+    }
+
+}
